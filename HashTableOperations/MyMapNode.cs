969273cfd0b69ddf3ade0ea6e632cf4cb9718ad7
1000007201 <nishant.sharma5504@gmail.com>
2022-02-15 07:55:31 +0530

    [Nishant] Add:Sentence Frequency Done

---
+++ HashTableOperations/MyMapNode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableOperations
+{
+    public class MyMapNode<K,V>
+    {
+        public int size;
+        public LinkedList<Keyvalue<K, V>>[] item;
+        
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.item = new LinkedList<Keyvalue<K,V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<Keyvalue<K, V>> linkedlist = GetLinkedList(position);
+            foreach(Keyvalue<K,V> item in linkedlist)
+            {
+                if(item.key.Equals(key))
+                {
+                    return item.value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<Keyvalue<K, V>> linkedlist = GetLinkedList(position);
+            Keyvalue<K,V> item = new Keyvalue<K, V> { key = key, value = value };
+            linkedlist.AddLast(item);
+
+        }
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<Keyvalue<K, V>> linkedlist = GetLinkedList(position);
+            bool ispresent = false;
+            Keyvalue<K,V> founditem = default(Keyvalue<K,V>);
+            foreach (Keyvalue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    ispresent = true;
+                    founditem = item;
+                }
+            }
+            if (ispresent)
+            {
+                linkedlist.Remove(founditem);
+            }
+        }
+        public LinkedList<Keyvalue<K,V>> GetLinkedList(int position)
+        {
+            LinkedList<Keyvalue<K,V>> linked = item[position];
+            if(linked == null)
+            {
+                linked = new LinkedList<Keyvalue<K,V>>();
+                item[position] = linked;
+            }
+            return linked;
+        }
+
+
+    }
+    public struct Keyvalue<k, v>
+    {
+        public k key { get; set; }
+        public v value { get; set; }
+    }
+}
---
+++ HashTableOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HashTableOperations;
+
+MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+hash.Add("0", "to");
+hash.Add("1", "be");
+hash.Add("2", "or");
+hash.Add("3", "not");
+hash.Add("4", "to");
+hash.Add("5", "be");
+
+Console.WriteLine($"5th index value:{hash.Get("5")}");
+Console.WriteLine($"2nd index value:{hash.Get("2")}");
